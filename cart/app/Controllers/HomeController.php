38aa2e4bdeed214f8d1d44e87c1cda3565989d7b
Tunji Akinbami <takinbami@springboardservices.ca>
2016-10-14 12:45:35 -0400

    new changes

--- cart/app/Controllers/HomeController.php
+++ cart/app/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace Cart\Controllers;
 
 use Slim\Views\Twig;
+use Cart\Models\Product;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
@@ -10,7 +11,13 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 class HomeController
 {
     
-    public function index(Request $request, Response $response, Twig $view){
+    public function index(Request $request, Response $response, Twig $view, Product $product){
+        
+        $products = $product->get();
+        
+        var_dump($products);
+        die();
+        
         return $view->render($response, 'home.twig');
     }
 }
